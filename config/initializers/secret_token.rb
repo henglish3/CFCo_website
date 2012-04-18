6daf41896cb5857d1a9d264160e8bd45bcc226b0
Harrison English <henglish3@gatech.edu>
2012-04-18 16:53:23 -0400

    started Fence Website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClementsFenceCo::Application.config.secret_token = '02d47d8abc063d34bfdfcd9fc03d7c3f2288078e830eaadce5fd0ddeade400927d1233d3fcb60a3bd403ee71a59db0e30eec186fe0d3fff9dfecf349534b16bd'
